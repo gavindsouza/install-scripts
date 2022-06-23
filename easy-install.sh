396c56f0d0cb15d67bee0ebada8062033ddc8971
Athul Cyriac Ajay <athul8720@gmail.com>
2022-06-23 12:32:03 +0530

    chore: changed sudo uses to root user level
    * Moved Sudo/Installations at root
    * Editable bench instance on frappe user
    * Argument for selecting frappe user
    * Use bench setup sudoers than writing to file
    * Font installation with apt for wkhtmtopdf(qt)

--- easy-install.sh
+++ easy-install.sh
@@ -5,7 +5,9 @@ ADMINPW="mypass"
 SITENAME="site1.local"
 BRANCH="develop"
 FRAPPEBRANCH="develop"
-while getopts ":m:a:s:b:f" OPTION; do
+FRAPPEUSER="frappe"
+
+while getopts ":m:a:s:b:f:u" OPTION; do
 
   case "$OPTION" in
     m)
@@ -23,17 +25,23 @@ while getopts ":m:a:s:b:f" OPTION; do
     f)
       FRAPPEBRANCH="$OPTARG"
       ;;
+    u)
+      FRAPPEUSER="$OPTARG"
+      ;;
     *)
-      echo "Usage: $(basename $0) [-a admin password] [-m mariadb password] [-s sitename] [-b ERPNext branch] [-f Frappe branch]"
+      echo "Usage: $(basename $0) [-a admin password] [-m mariadb password] [-s sitename] [-b ERPNext branch] [-f Frappe branch] [-u frappe user]"
       exit 1
       ;;
   esac
 done
 
-useradd -m -s /usr/bin/bash frappe
-echo 'frappe ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers
-sudo -i -u frappe bash << EOF
-source ~/.profile
+useradd -m -s /usr/bin/bash $FRAPPEUSER
+
+sudo apt install -y python3-pip fontconfig libxrender1 libxext6 xfonts-75dpi xfonts-base wget
+
+sudo pip install frappe-bench
+
+sudo bench setup sudoers
 
 # Install docker if it doesn't exist
 if ! command -v docker &> /dev/null
@@ -41,6 +49,30 @@ then
     wget https://get.docker.com/ -O install-docker.sh
     sh install-docker.sh
 fi
+
+# Start docker daemon
+sudo systemctl start docker 
+sudo gpasswd -a $FRAPPEUSER docker
+
+# Start mariadb in Docker
+wget -O ~/.my.cnf https://gist.githubusercontent.com/athul/96c7e86c7e6e70c7f8d4e87e49c95f32/raw/fa68699858b68d1676134f3d4c5c5f045a4872c9/my.cnf
+sudo docker run --name frappe-mariadb -v ~/.my.cnf:/etc/mysql/my.cnf -e MYSQL_ROOT_PASSWORD=$MDBPW -p 3306:3306 -d docker.io/library/mariadb:10.6
+
+# Install wkhtmltopdf
+echo "Installing wkhtmltopdf"
+
+wget -O /tmp/wkhtmltopdf.deb https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.focal_amd64.deb
+
+sudo dpkg -i /tmp/wkhtmltopdf.deb
+
+sudo rm -rf /tmp/wkhtmltopdf.deb
+
+
+# echo 'frappe ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers
+
+sudo -i -u $FRAPPEUSER sh << EOF
+source ~/.profile
+
 # Install Nix if it doesn't exist
 if ! command -v nix-env &> /dev/null
 then
@@ -50,7 +82,9 @@ fi
 
 # Restart Shell
 
-exec $SHELL
+# exec $SHELL
+
+source ~/.bashrc
 
 
 # Install the required packages with Nix
@@ -70,48 +104,19 @@ nix-env -iA \
 
 # Install bench
 echo "Installing Bench"
-sudo apt install -y python3-pip fontconfig
-sudo pip install frappe-bench 
 
-
-# Export Path if non-existent
-
-export PATH="$HOME/.local/bin:$PATH"
-
-# Install base fonts for wkhtmltopdf
-
-git clone --progress --depth 1 https://github.com/frappe/fonts.git /tmp/fonts 
-
-sudo rm -rf /etc/fonts && mv /tmp/fonts/etc_fonts /etc/fonts
-sudo rm -rf /usr/share/fonts && mv /tmp/fonts/usr_share_fonts /usr/share/fonts
-fc-cache -f 
-
-# Install wkhtmltopdf
-echo "Installing wkhtmltopdf"
-
-curl -L  https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.focal_amd64.deb -o /tmp/wkhtmltopdf.deb
-
-sudo dpkg -i /tmp/wkhtmltopdf.deb
-
-sudo rm -rf /tmp/wkhtmltopdf.deb /tmp/fonts
-
-# Start docker daemon
-sudo systemctl start docker 
-sudo gpasswd -a "${USER}" docker
-
-# Start mariadb in Docker
-wget https://gist.githubusercontent.com/athul/x/raw/56877ad792fe2656931403c74dadeb3a80b33689/my.cnf -o /tmp/my.cnf
-sudo docker run --name frappe-mariadb -v /tmp/my.cnf:/etc/mysql/my.cnf -e MYSQL_ROOT_PASSWORD=$MDBPW -p 3306:3306 -d docker.io/library/mariadb:10.6
-
+git clone https://github.com/frappe/bench ~/.bench 
+#Editable local install
+pip install -e ~/.bench 
 
 # Init bench
 echo "Installing Bench and Setting Up ERPNext for production"
-bench init /home/frappe/frappe-bench --version $FRAPPEBRANCH
+bench init /home/$FRAPPEUSER/frappe-bench --version $FRAPPEBRANCH
 
 echo "Creating a new site with $SITENAME"
-cd /home/frappe/frappe-bench
-bench new site $SITENAME  --admin-password $ADMINPW --mariadb-root-password $MDBPW
+cd /home/$FRAPPEUSER/frappe-bench
+bench new site $SITENAME --admin-password $ADMINPW --mariadb-root-password $MDBPW
 bench get-app erpnext https://github.com/frappe/erpnext --branch $BRANCH
 bench --site $SITENAME install-app erpnext
-sudo bench setup production $USER --yes
+sudo bench setup production $FRAPPEUSER --yes
 EOF
